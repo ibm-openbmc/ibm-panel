d73be2790cc88d6b1b9037877df9d05abe72b9cc
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-05-20 10:28:29 -0500

    Register handle for post code property change
    This commit implements functionality to register and listen for
    property change signal on Dbus for postcode property under
    xyz.openbmc_project.State.Boot.Raw interface.
    
    The SRC value stored in this property will be used to display
    progress code on Op-Panel display unit.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- src/panel_app_main.cpp
+++ src/panel_app_main.cpp
@@ -1,11 +1,45 @@
+#include <iostream>
 #include <panel_app.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+
+void progressCodeCallBack(sdbusplus::message::message& msg)
+{
+    using PostCode = std::tuple<uint64_t, std::vector<uint8_t>>;
+
+    std::string interface{};
+    std::string propVal{};
+    std::map<std::string, std::variant<PostCode>> propertyMap;
+
+    msg.read(interface, propertyMap);
+
+    // property we are looking for.
+    const auto it = propertyMap.find("Value");
+    if (it != propertyMap.end())
+    {
+        if (auto postCodeData = std::get_if<PostCode>(&(it->second)))
+        {
+            uint64_t src = std::get<0>(*postCodeData);
+            std::ostringstream os;
+            os << src;
+            propVal = os.str();
+
+            std::cout << "Progress code SRC value = " << propVal << std::endl;
+        }
+        else
+        {
+            std::cerr << "Progress code Data error" << std::endl;
+        }
+        // TODO:
+        // send the data to display?
+    }
+}
 
 int main(int, char**)
 {
     boost::asio::io_context io;
     auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+
     conn->request_name("com.ibm.PanelApp");
 
     auto server = sdbusplus::asio::object_server(conn);
@@ -14,6 +48,18 @@ int main(int, char**)
         server.add_interface("/com/ibm/panel_app", "com.ibm.panel");
 
     iface->register_method("Display", display);
+
+    // register to progress code property change.
+    auto progressCodeSignal = std::make_unique<sdbusplus::bus::match::match>(
+        *conn,
+        sdbusplus::bus::match::rules::propertiesChanged(
+            "/xyz/openbmc_project/state/boot/raw0",
+            "xyz.openbmc_project.State.Boot.Raw"),
+        progressCodeCallBack);
+
+    io.run();
+
+    // ends here
 
     // Create the Transport class
     // Create the ButtonHandler class
