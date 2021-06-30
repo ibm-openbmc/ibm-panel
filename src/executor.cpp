54eda6a7ac5179528da0363d8e84afd3ae48c897
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-06-30 02:01:35 -0500

    Implementation of functionality 20
    This function displays the machine type and model, the custom card
    identification number (CCIN) and System Serial number.
    
    Sample output:
    L1 : 9105-22A   2E2D
    L2 : SIMP10R
    
    Change-Id: I10e3ef376705fd3f7eb884ee11c8240919a81f28
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,25 +1,71 @@
 #include "executor.hpp"
 
+#include "const.hpp"
+#include "utils.hpp"
+
 namespace panel
 {
 
-void Executor::executeFunction(
-    const panel::types::FunctionNumber funcNumber,
-    const panel::types::FunctionalityList& subFuncNumber)
+void Executor::executeFunction(const types::FunctionNumber funcNumber,
+                               const types::FunctionalityList& subFuncNumber)
 {
+    // to remove warning. will be removed with subsequent commits.
+    (void)subFuncNumber;
+
     // test output, to be removed
-    std::cout << funcNumber << std::endl;
-    std::cout << subFuncNumber.at(0) << std::endl;
+    switch (funcNumber)
+    {
+        case 20:
+            execute20();
+
+        default:
+            break;
+    }
 }
 
-void Executor::execute01()
+void Executor::execute20()
 {
-    // TODO implementation
-}
+    auto res = utils::readBusProperty<std::variant<std::string>>(
+        "xyz.openbmc_project.Inventory.Manager",
+        "/xyz/openbmc_project/inventory/system",
+        "xyz.openbmc_project.Inventory.Decorator.Asset", "SerialNumber");
 
-void Executor::execute02()
-{
-    // TODO implementation
+    std::string line1(16, ' ');
+    std::string line2(16, ' ');
+
+    const auto serialNumber = std::get_if<std::string>(&res);
+    if (serialNumber != nullptr)
+    {
+        line2.replace(0, (*serialNumber).length(), *serialNumber);
+    }
+
+    // reading machine model type
+    auto resValue = utils::readBusProperty<std::variant<types::Binary>>(
+        "xyz.openbmc_project.Inventory.Manager",
+        "/xyz/openbmc_project/inventory/system/chassis/motherboard",
+        "com.ibm.ipzvpd.VSYS", "TM");
+
+    const auto propData = std::get_if<types::Binary>(&resValue);
+
+    if (propData != nullptr)
+    {
+        line1.replace(0, constants::tmKwdDataLength,
+                      reinterpret_cast<const char*>(propData->data()));
+    }
+
+    // reading CCIN
+    res = utils::readBusProperty<std::variant<std::string>>(
+        "xyz.openbmc_project.Inventory.Manager",
+        "/xyz/openbmc_project/inventory/system/chassis/motherboard",
+        "xyz.openbmc_project.Inventory.Decorator.Asset", "Model");
+
+    const auto model = std::get_if<std::string>(&res);
+    if (model != nullptr)
+    {
+        line1.replace(11, constants::ccinDataLength, *model);
+    }
+
+    utils::sendCurrDisplayToPanel(line1, line2, transportObj);
 }
 
 } // namespace panel
