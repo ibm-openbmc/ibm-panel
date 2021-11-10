54a13ef4204dc08a026d155a72162ed7ec0ae52d
Giridhari Krishna <giridharikrishnan@gmail.com>
2021-11-10 17:49:09 +0530

    Fixing clang errors in panel_app_test (#62)
    Co-authored-by: giridhari <giridhari@giridharis-MacBook-Pro.local>

--- test/panel_app_test.cpp
+++ test/panel_app_test.cpp
@@ -6,5 +6,5 @@ TEST(PanelApp, display)
 {
     // Good path.
     // To add later via issue #3353
-    //EXPECT_EQ(true, display("BLAH", "BLAH1"));
+    // EXPECT_EQ(true, display("BLAH", "BLAH1"));
 }
