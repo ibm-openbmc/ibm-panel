d5379595aa9ff1a99516969e0ca4a34ca5caab6c
GiridhariKrishna <47595253+GiridhariKrishna@users.noreply.github.com>
2022-02-15 10:08:12 +0530

    Tool for simulating panel input (#97)
    This commit implements paneltool exe to simulate panel button
    event and panel behaviour corresponding to the input in a
    given scenario.
    
    Sample command for up button event:
    paneltool -b UP
    
    paneltool --help for more details.
    
    Sample command (Panel at function number 01):
    ./paneltool -b EXECUTE
    
    output:
    L1 : 01     N    PVM
    L2 :             T

---
+++ tools/src/panel_tool.cpp
@@ -0,0 +1,35 @@
+#include "dbus_call.hpp"
+
+#include <CLI/CLI.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::string input{};
+    CLI::App app{"Command line tool for simulating panel functions "};
+    auto state =
+        app.add_option(
+               " -b", input,
+               " Simulating button press"
+               " Increment/Decrement/Execute with UP/DOWN/EXECUTE respectively")
+    CLI11_PARSE(app, argc, argv);
+
+    try
+    {
+        if (*state)
+        {
+            panel::tool::btnEventDbusCall(input);
+        }
+        else
+        {
+            throw std::runtime_error(
+                "One of the valid options is required. Refer "
+                "--help for list of options.");
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what();
+    }
+    return 0;
+}
