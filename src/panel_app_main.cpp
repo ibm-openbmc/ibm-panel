661453cceb06b11ee0089b1445eeb34572509b66
Jinu Joy Thomas <jinu.joy.thomas@in.ibm.com>
2022-08-01 11:24:32 +0530

    Fix write Error displays when Hot-plugging the panel out (#153)
    Since we have a 5 second presence checker , we can check the
    write failure within 5 second intervals so as to see if the
    write failure has happened during the Hot plugging out of
    the panel or not. If so we do not log that error.
    
    Signed-off-by: Jinu Joy Thomas <jinu.joy.thomas@in.ibm.com>

--- src/panel_app_main.cpp
+++ src/panel_app_main.cpp
@@ -23,36 +23,6 @@ panel::types::PanelDataMap baseDataMap = {
      {panel::constants::baseDevPath, panel::constants::devAddr,
       panel::constants::everBaseDbusObj}}};
 
-panel::types::PanelDataMap lcdDataMap = {
-    {panel::constants::rain2s2uIM,
-     {panel::constants::rainLcdDevPath, panel::constants::devAddr,
-      panel::constants::rainLcdDbusObj}},
-    {panel::constants::rain2s4uIM,
-     {panel::constants::rainLcdDevPath, panel::constants::devAddr,
-      panel::constants::rainLcdDbusObj}},
-    {panel::constants::rain1s4uIM,
-     {panel::constants::rainLcdDevPath, panel::constants::devAddr,
-      panel::constants::rainLcdDbusObj}},
-    {panel::constants::everestIM,
-     {panel::constants::everLcdDevPath, panel::constants::devAddr,
-      panel::constants::everLcdDbusObj}}};
-
-std::string getIM()
-{
-    auto im = panel::utils::readBusProperty<std::variant<panel::types::Binary>>(
-        panel::constants::inventoryManagerIntf, panel::constants::systemDbusObj,
-        panel::constants::imInterface, panel::constants::imKeyword);
-    if (auto imVector = std::get_if<panel::types::Binary>(&im))
-    {
-        return panel::utils::binaryToHexString(*imVector);
-    }
-    else
-    {
-        std::cerr << "\n Failed querying IM property from dbus" << std::endl;
-    }
-    return "";
-}
-
 std::string getInputDevicePath(const std::string& imValue)
 {
     if (imValue == panel::constants::rain2s2uIM ||
@@ -70,38 +40,23 @@ std::string getInputDevicePath(const std::string& imValue)
     return "/dev/input/by-path/platform-1e78a080.i2c-bus-event-joystick";
 }
 
-bool getPresentProperty(const std::string& imValue)
-{
-    auto present = panel::utils::readBusProperty<std::variant<bool>>(
-        panel::constants::inventoryManagerIntf,
-        std::get<2>((lcdDataMap.find(imValue))->second),
-        panel::constants::itemInterface, "Present");
-    if (auto p = std::get_if<bool>(&present))
-    {
-        return *p;
-    }
-    else
-    {
-        std::cerr << "\n Failed querying Present property from dbus."
-                  << std::endl;
-    }
-    return false;
-}
-
 void getLcdDeviceData(std::string& lcdDevPath, uint8_t& lcdDevAddr,
                       std::string& lcdObjPath, const std::string& imValue)
 {
-    if (lcdDataMap.find(imValue) ==
-        lcdDataMap.end()) // assume the system is tacoma
+    if (panel::utils::lcdDataMap.find(imValue) ==
+        panel::utils::lcdDataMap.end()) // assume the system is tacoma
     {
         lcdDevPath = panel::constants::tacomaLcdDevPath;
         lcdDevAddr = panel::constants::devAddr;
     }
     else
     {
-        lcdDevPath = std::get<0>((lcdDataMap.find(imValue))->second);
-        lcdDevAddr = std::get<1>((lcdDataMap.find(imValue))->second);
-        lcdObjPath = std::get<2>((lcdDataMap.find(imValue))->second);
+        lcdDevPath =
+            std::get<0>((panel::utils::lcdDataMap.find(imValue))->second);
+        lcdDevAddr =
+            std::get<1>((panel::utils::lcdDataMap.find(imValue))->second);
+        lcdObjPath =
+            std::get<2>((panel::utils::lcdDataMap.find(imValue))->second);
     }
 }
 
@@ -118,7 +73,7 @@ int main(int, char**)
         std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
             server.add_interface("/com/ibm/panel_app", "com.ibm.panel");
 
-        const std::string imValue = getIM();
+        const std::string imValue = panel::utils::getSystemIM();
 
         std::string lcdDevPath{}, lcdObjPath{};
         uint8_t lcdDevAddr;
@@ -142,7 +97,8 @@ int main(int, char**)
         // Listen to lcd panel presence always for both rainier and everest
         std::unique_ptr<panel::PanelPresence> presence;
 
-        if (lcdDataMap.find(imValue) != lcdDataMap.end())
+        if (panel::utils::lcdDataMap.find(imValue) !=
+            panel::utils::lcdDataMap.end())
         {
             presence = std::make_unique<panel::PanelPresence>(lcdObjPath, conn,
                                                               lcdPanel);
@@ -155,7 +111,8 @@ int main(int, char**)
              * change from false to true; but the transport key is still
              * true(unchanged). To maintain data accuracy get the "Present"
              * property from dbus and set the transport key again.*/
-            lcdPanel->setTransportKey(getPresentProperty(imValue));
+            lcdPanel->setTransportKey(
+                panel::utils::getLcdPanelPresentProperty(imValue));
         }
         else
         {
