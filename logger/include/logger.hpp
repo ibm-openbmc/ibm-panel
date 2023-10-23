c0036ce0ddba657bd607cbf5de2ad192fc6a9f65
RekhaAparna01 <vrekhaaparna@ibm.com>
2023-10-23 03:15:49 -0500

    Define's API's for logger library
    The logger library collects the log traces and stores them in the
    buffer associated to a specific application.
    The buffer is useful in debug process for the error tracking
    
    Test:
    tested by adding traces to the library
    
    Test output:
    2023-11-14 04:04:00 [INFO] : panel_app_main.cpp:71 - Test Message1
    2023-11-14 04:04:00 [INFO] : panel_app_main.cpp:72 - Test Message2
    
    Change-Id: I56e026bf95b3f8ecb1be7b5ed60242c39bc147a0
    Signed-off-by: RekhaAparna01 <vrekhaaparna@ibm.com>

--- logger/include/logger.hpp
+++ logger/include/logger.hpp
@@ -1,20 +1,48 @@
 #pragma once
 
+#include <filesystem>
 #include <iostream>
+#include <list>
+#include <map>
 #include <source_location>
+#include <sstream>
 
-namespace logging
+namespace Logger
 {
 
-/**
- *@brief An API to append log messages to a buffer
- *This API is called to log message. It also appends the
- *file name and line number of the mesaage.
- *
- *param[in] - Information that we want to log
- *param[in] - Object of source_location class
- */
-void logMessage(std::string message, const std::source_location& location =
-                                         std::source_location::current());
+enum Loglevel
+{
+    INFO,
+    CRITICAL
+};
 
-} // namespace logging
+// Maximum size of buffer is 8k bytes
+const size_t maxBufferSize = 8000;
+inline std::list<std::string> circularBuffer;
+
+/**
+ *@brief Appends data to the buffer
+ *
+ *This API is called to log message to the buffer.
+ *It checks the log level and appends information like
+ *line number, file name, timestamp to the message.
+ *
+ *@param[in] level - loglevel of the message
+ *@param[in] message - Information to be logged
+ *@param[in] location - object of the source_location class
+ */
+void logMessage(
+    const Loglevel level, const std::string& message,
+    const std::source_location& location = std::source_location::current());
+
+/**
+ *@brief Appends data to buffer with INFO as default Log Level
+ *
+ *@param[in] message - Information to be logged
+ *@param[in] loaction - object of the source_location class
+ */
+void logMessage(
+    const std::string& message,
+    const std::source_location& location = std::source_location::current());
+
+}; // namespace Logger
--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -1,18 +1,59 @@
 #include "logger.hpp"
 
-#include <filesystem>
-#include <iostream>
-#include <sstream>
-#include <string>
+#include <ctime>
 
-namespace logging
+namespace Logger
 {
-std::ostringstream logMsg;
-void logMessage(std::string message, const std::source_location& location)
+
+static std::map<Loglevel, std::string> logLevelMap = {
+    {Loglevel::INFO, "INFO"}, {Loglevel::CRITICAL, "CRITICAL"}};
+
+size_t byteCount = 0;
+
+/**
+ * @brief Generates a timestamp string
+ *
+ * Generates a timestamp string representing the current time
+ * in the format "YYYY-MM-DD HH:MM:SS"
+ *
+ * @return A string containing the formatted timestamp
+ */
+static std::string timestamp()
 {
-    logMsg << "File: "
-           << std::filesystem::path(location.file_name()).filename().string()
-           << " line: " << location.line() << " " << message << "\n";
+    time_t t = time(nullptr);
+    tm Time{};
+    gmtime_r(&t, &Time);
+    std::string timeStamp;
+    timeStamp.resize(30, '\0');
+    size_t currentTime = strftime(timeStamp.data(), timeStamp.size(),
+                                  "%Y-%m-%d %H:%M:%S", &Time);
+    timeStamp.resize(currentTime);
+    return timeStamp;
 }
 
-} // namespace logging
+void logMessage(const Loglevel level, const std::string& message,
+                const std::source_location& location)
+{
+    std::string tmpString =
+        timestamp() + " [" + logLevelMap[level] + "]" + " : " +
+        std::filesystem::path(location.file_name()).filename().string() + ":" +
+        std::to_string(location.line()) + " - " + message + "\n";
+    while (byteCount + tmpString.size() > maxBufferSize)
+    {
+        byteCount = byteCount - (*circularBuffer.begin()).size();
+        circularBuffer.erase(circularBuffer.begin());
+    }
+    if (Loglevel::INFO == level)
+    {
+        circularBuffer.push_back(tmpString);
+        byteCount = byteCount + tmpString.size();
+    }
+}
+
+void logMessage(const std::string& message,
+                const std::source_location& location)
+{
+    logMessage(Loglevel::INFO, message, location);
+}
+
+} // namespace Logger
