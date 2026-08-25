b1a6f9d06c515a9fda02585d355dc83679510a35
Anupama B R <anupama.b.r1@ibm.com>
2026-08-25 13:50:07 +0000

    Add Transport class skeleton
    The Transport class provides the communication layer with the panel
    micro-controller. All panel commands are delivered as raw I2C writes
    through this class.
    
    Removed all methods in the Transport class in this commit, except empty
    constructor and destructor, remaining functionality will be added in
    subsequent commit.
    Added initPanel API in main.cpp to instantiate Transport at start-up.
    
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- include/transport.hpp
+++ include/transport.hpp
@@ -4,185 +4,35 @@
 
 #include <unistd.h>
 
+#include <string>
+
 namespace panel
 {
 /** @class Transport
- * @brief The Transport class is to communicate with the i2c devices.
+ *
+ * @brief The Transport class is to communicate with the i2c devices
+ *
  * When the BMC needs to communicate with the panel microcontroller, raw i2c
  * writes are made using transport class api.
  */
 class Transport
 {
   public:
-    /**
-     * A Default Constructor
-     * For testing purpose
-     */
-    Transport() :
-        devPath(" "), devAddress(0), panelType(panel::types::PanelType::LCD)
-    {
-        setTransportKey(false);
-    }
+    /** @brief Deleted special members */
+    Transport(const Transport&) = delete;
+    Transport& operator=(const Transport&) = delete;
+    Transport& operator=(Transport&&) = delete;
+    Transport(Transport&&) = delete;
 
     /**
-     * A Constructor
-     * Initialise the transport class object with the right panel device path
-     * and device address based on the system type.
-     */
-    Transport(const std::string& devPath, const uint8_t& devAddr,
-              const types::PanelType& type, const std::string& objectPath) :
-        devPath(devPath), devAddress(devAddr), panelType(type),
-        fruPath(objectPath)
-    {
-        panelI2CSetup();
-    }
-
-    /**
-     * A Destructor
-     * Closes the valid file descriptor when the object goes out of scope.
-     */
-    ~Transport()
-    {
-        if (panelFileDescriptor != -1)
-        {
-            close(panelFileDescriptor);
-        }
-    }
-
-    /** @brief Write to the panel micro controller via I2C bus.
-     * This api does raw i2c writes of the panel commands to the panel's micro
-     * controller.
-     * @param[in] buffer - data that needs to be sent to the panel.
-     */
-    void panelI2CWrite(const types::Binary& buffer) const;
-
-    /** @brief Method to set the transport key
-     * The transportKey boolean tells if the panel i2c bus is ready to use or
-     * not. This method is to flip the key value to true/false based on the end
-     * user requirement.
-     * @param[in] keyValue - boolean to tell whether to activate the key or not.
-     */
-    void setTransportKey(bool keyValue);
-
-    /** @brief API to setup panel's button operational characteristics. */
-    void doButtonConfig();
-
-    /** @brief Method to check and return the current status of transport key.
-     * @return transportKey
-     */
-    inline bool isTransportKeyEnabled() const
-    {
-        return transportKey;
-    }
-
-    /** @brief Method to get panel type
-     * @return panel type(LCD/BASE)
-     */
-    inline types::PanelType getPanelType() const
-    {
-        return panelType;
-    }
-
-  private:
-    /** @brief The panel's file descriptor */
-    int panelFileDescriptor = -1;
-
-    /** @brief Panel device path */
-    const std::string devPath;
-
-    /** @brief Panel device address */
-    const uint8_t devAddress;
-
-    /** @brief Key to check the availability of transport class.
-     * The transportKey tells if the panel i2c bus is ready to use or not.
-     * If key value is true, the transport class can be used to access the panel
-     * i2c bus. False otherwise.
-     */
-    bool transportKey = false;
-
-    /** @brief Panel type (base/lcd) */
-    const types::PanelType panelType;
-
-    /** @brief i2cAddress and devAddress are same in value but differs in type.
-     * This is required to log CALLOUT_IIC_ADDR in PEL.*/
-    std::string i2cAddress{};
-
-    /** @brief Base/LCD panel FRU path */
-    const std::string fruPath;
-
-    /** @brief Establish panel i2c connection
-     * This api establishes the i2c bus connection to the panel micro
-     * controller.
-     */
-    void panelI2CSetup();
-
-    /** @brief API to do soft reset.
-     * The Panel Code Soft Reset command is used to perform a soft reset of
-     * the Panel micro-controller. This will re-initialize the Panel micro-code
-     * to its start-up values. A delay of 100milliseconds is added after the
-     * soft reset operation.
-     */
-    void doSoftReset();
-
-    /**
-     * @brief API to get the panel out of a bootloader hang
+     * @brief Default constructor
      *
-     * Due to a bug in some levels of the microcode, the panel can sometimes be
-     * stuck in the bootloader. This API attempts to recover from the situation
-     * by forcing the bootloader to jump to the main panel program.
+     * Creates a Transport instance with no device details, intended to use for
+     * testing.
      */
-    void checkAndFixBootLoaderBug();
+    Transport() noexcept;
 
-    /**
-     * @brief API to read panel current version
-     *
-     * This method reads 6 bytes of panel's current version and stores in the io
-     * reference argument.
-     *
-     * @param[out] - Reference to byte vector to store the current version.
-     *
-     * @return true if version read successfully, false otherwise.
-     */
-    bool readPanelVersion(types::Binary& currVersion) const;
-
-    /**
-     * @brief API which does panel firmware update
-     * This method does both LCD and base firmware code update with their latest
-     * code respectively, if the existing panel firmware is not the latest.
-     */
-    void doFWUpdate() const;
-
-    /**
-     * @brief API to go to boot loader from main program
-     *
-     * @return true if its a successful jump to bootloader, false otherwise.
-     */
-    bool gotoBootloader() const;
-
-    /**
-     * @brief API which updates the panel FW with the latest.
-     *
-     * @return true if its a successful updation, false otherwise.
-     */
-    bool updateFlash() const;
-
-    /**
-     * @brief API to go main program from bootloader.
-     *
-     * @return true if its a successful jump to main program, false otherwise.
-     */
-    bool gotoMainProgram() const;
-
-    /** @brief Log error related to code update failure.
-     *
-     * @param[in] description - Error description.
-     * @param[in] err - errno.
-     * @param[in] control - Says if panel control reaches Main program or Boot
-     * loader.
-     * @param[in] pelIntf - PEL interface.
-     */
-    void logCodeUpdateError(const std::string& description, const int err,
-                            const std::string& control,
-                            const std::string& pelIntf) const;
+    /** @brief Destructor */
+    ~Transport() noexcept;
 };
 } // namespace panel
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include "const.hpp"
+#include "transport.hpp"
 
 #include <boost/asio/io_context.hpp>
 #include <memory>
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+
+/**
+ * @brief Initialise the panel subsystem.
+ *
+ * Creates a Transport instance and other panel-level objects required at
+ * start-up.
+ */
+void initPanel() noexcept
+{
+    try
+    {
+        // TODO: Pass real devPath, devAddr and fruPath once available.
+        auto transport = std::make_shared<panel::Transport>();
+    }
+    catch (const std::exception& ex)
+    {
+        lg2::error("Failed to initialise the panel, reason: {ERROR}", "ERROR",
+                   ex);
+        // TODO: log a critical PEL
+    }
+}
 
 int main()
 {
@@ -23,6 +45,8 @@ int main()
         std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
             server.add_interface(panel::constants::panelObjectPath,
                                  panel::constants::panelInterface);
+
+        initPanel();
 
         iface->initialize();
 
